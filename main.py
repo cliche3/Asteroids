f71596129d9442b2645b882b38b2cca15a9aad0f
cliche3 <ilonka_albert@outlook.com>
2025-06-23 20:25:25 +0200

    player can rotate

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
-    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.update(dt)
 
         screen.fill("black")
         player.draw(screen)
